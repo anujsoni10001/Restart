262e4e88646aa7c15e7405ea28e0f012c6785675
Anuj Soni <anujsoni10001@gmail.com>
2022-03-10 14:34:18 +0530

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Anuj Soni on 10/03/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
